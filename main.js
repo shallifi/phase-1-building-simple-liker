56864288e85c97b123c141a9589d2c780026e158
Benjamin <shallifi@hotmail.com>
2022-04-08 00:16:48 -0400

    Incomplete but tried

--- main.js
+++ main.js
@@ -4,8 +4,60 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const loveHearts = document.querySelector("like-glyph");
+
+console.log(loveHearts)
+// loveHearts.addEventListener("click", heartCall);
+
+//class to the error modal in the HTML so it does not appear when the page first loads
+
+// *******changed the html to add class = hidden***************
 
 
+//Submit a request to a mocked-up server
+
+function heartCall(e) {
+  const hearts = e.target;
+  mimicServerCall("fake")
+  .then(function(){
+    if (hearts.innerText === EMPTY_HEART) {
+      hearts.innerText = FULL_HEART;
+      // activated-heart on css gives red color
+      hearts.className = "activated-heart";
+    } else {
+      hearts.innerText = EMPTY_HEART;
+      hearts.className = "";
+    }
+  })
+  .catch(function(error) {
+    const modal = document.querySelector("#modal");
+      modal.className = "";
+      modal.innerText = error;
+      setTimeout(() => modal.className = "hidden", 3000);
+  });
+}
+
+
+//Set up an event listener to respond to a user action
+// for (const glyph of loveHearts) {
+// loveHearts.addEventListener("click", heartCall);
+// }
+
+// Update the DOM based on the mock server's response
+
+
+
+
+
+
+
+//////////////////////////////////////////////////
+
+// const modal = document.getElementById("modal")
+//   .addEventListener('click', function() {
+//   modal.hidden("please") = true;
+//   modal.hidden("it is wrong") = false;
+// }, false);
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
